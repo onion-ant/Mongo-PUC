14448c7cb3ab4f6b801357b5783c1ba3ece486a4
Antonio Henrique <antoniohenriquebv@gmail.com>
2024-10-19 11:57:45 -0300

    chore: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - Trabalho Mongo')
+    .setDescription(
+      'Documentação gerada pelo swagger para interação com api nest',
+    )
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api', app, document);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
